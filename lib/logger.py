02d8a11b57c9e254ed2083f76c7a89c088a78056
Tim <tim@timschroeder.net>
2018-11-08 23:08:29 +0100

    Update .gitignore to reflect new files

--- lib/logger.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-def log(*args):
-    msg = ""
-    for a in args:
-        msg += a
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print('[' + now + ']: ' + msg)
